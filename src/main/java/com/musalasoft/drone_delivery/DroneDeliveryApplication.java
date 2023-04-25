3876d7077386eb967337e3b7cc0b43471c42df52
zingwena-softwares <warrenzingwena@gmail.com>
2023-04-25 16:07:32 +0200

    first commit

---
+++ src/main/java/com/musalasoft/drone_delivery/DroneDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.musalasoft.drone_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musalasoft/drone_delivery/DroneDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.drone_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
